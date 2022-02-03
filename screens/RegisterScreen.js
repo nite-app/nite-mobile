212b3c1e05dd0d330c6d5a339e6071017a58116a
Dags-24 <davidedado04@gmail.com>
2022-02-03 22:18:11 +0100

    responsive login

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -14,6 +14,8 @@ import { useNavigation } from "@react-navigation/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 
+const height = Dimensions.get('window').height;
+
 const RegisterScreen = (props) => {
   const navigation = useNavigation();
 
@@ -112,58 +114,76 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: "80%",
-    marginTop: 50,
-    marginBottom: 50,
+    //marginTop: 50,
+    marginTop: height*0.061576,
+    //marginBottom: 50,
+    marginBottom: height*0.061576,
   },
   input: {
     backgroundColor: "white",
-    marginBottom: 15,
-    paddingVertical: 20,
-    paddingHorizontal: 25,
-    borderRadius: 20,
-    fontSize: 18,
+    //marginBottom: 15,
+    marginBottom: height*0.01847,
+    //paddingVertical: 20,
+    paddingVertical: height*0.0246,
+    //paddingHorizontal: 25,
+    paddingHorizontal: height*0.03079,
+    //borderRadius: 20,
+    borderRadius: height*0.0246,
+     //fontSize: 18,
+    fontSize: height*0.0222,
   },
   buttonContainer: {
     width: "80%",
   },
   button: {
-    padding: 22,
+    //padding: 22,
+    padding: height*0.0271,
     backgroundColor: "#FF6768",
-    borderRadius: 20,
-    marginBottom: 15,
+    //borderRadius: 20,
+    borderRadius: height*0.0246,
+    //marginBottom: 15,
+    marginBottom: height*0.01847,
     alignItems: "center",
   },
   buttonText: {
-    fontSize: 18,
+    //fontSize: 18,
+    fontSize: height*0.0222,
     color: "#ffffff",
     fontWeight: "600",
   },
   buttonOutline: {},
   buttonOutlineText: {
-    fontSize: 18,
+    //fontSize: 18,
+    fontSize: height*0.0222,
     color: "#ffffff",
     fontWeight: "600",
   },
   textContainer: {
     alignItems: "center",
     width: "70%",
-    marginTop: 100,
+    //marginTop: 100,
+    marginTop: height*0.12315,
   },
   Title: {
-    fontSize: 45,
+    //fontSize: 45,
+    fontSize: height*0.055,
     fontWeight: "bold",
-    marginBottom: 15,
+    //marginBottom: 15,
+    marginBottom: height*0.01847,
   },
   Subtitle: {
-    fontSize: 26,
+    //fontSize: 26,
+    fontSize: height*0.032,
     textAlign: "center",
   },
   navigateLogin: {
-    marginTop: 120,
+    //marginTop: 120,
+    marginTop: height*0.234375,
   },
   loginBtn: {},
   loginTxt: {
-    fontSize: 16,
+    //fontSize: 16,
+    fontSize: height*0.0197,
     color: "blue",
     fontWeight: "600",
   },
